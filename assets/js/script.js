14d71781143c8dcb7efe9755d0f0a1eaf1f07bca
Sara Gaya <saracgaya@gmail.com>
2022-01-23 22:14:44 -0500

    added dark mode!

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+//IIFE, allows code to be executed immediately in its own scope
+(function () {
+  checkMode();
+  setMode();
+})();
+
+function checkMode() {
+  const body = document.body;
+  const darkMode = localStorage.getItem("darkMode");
+  console.log(darkMode);
+  darkMode === "true"
+    ? body.classList.add("dark-mode")
+    : body.classList.remove("dark-mode");
+}
+
+function setMode() {
+  const body = document.body;
+  const modeSwitch = document.getElementsByClassName("mode-switch")[0];
+  modeSwitch.addEventListener("click", (e) => {
+    e.preventDefault();
+    body.classList.toggle("dark-mode");
+
+    body.classList.contains("dark-mode")
+      ? localStorage.setItem("darkMode", true)
+      : localStorage.setItem("darkMode", false);
+  });
+}
